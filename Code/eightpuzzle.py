87fab17ca749c439d055af76dea368e87368465e
ZaneCarlson <146386094+ZaneCarlson@users.noreply.github.com>
2025-10-07 16:04:24 -0400

    init commit

--- Code/eightpuzzle.py
+++ Code/eightpuzzle.py
@@ -73,7 +73,7 @@ class EightPuzzleState:
 
         >>> EightPuzzleState([0, 1, 2, 3, 4, 5, 6, 7, 8]).isGoal()
         True
-
+        
         >>> EightPuzzleState([1, 0, 2, 3, 4, 5, 6, 7, 8]).isGoal()
         False
         """
